63a27fac23dd725a096ddfa291845980b4d0bcc7
Martin Bram Moreinis <martin.moreinis@schoolsofwestfield.org>
2024-11-26 16:41:22 +0000

    merged

--- script.js
+++ script.js
@@ -1,25 +1,51 @@
-var round = 1;
+/* Function getRounds
+ * pulls rounds from the input box on the home page and sends to setRounds
+ * @param = none
+ * @return = none
+ */
 function getRounds(){
     let rounds = document.getElementById("rounds").value;
     setRounds(rounds);
 }
 
+/* Function getRounds
+ * Checks if rounds are odd. If even, warning message. 
+ * Otherwise, sets round to 1, stores rounds and round in localStorage and loads chooser.html. 
+ * @param = founds
+ * @localStorage => round, rounds
+ * @return = none
+ */
 function setRounds(rounds){
     if (rounds % 2 == 0) {
         alert("must be odd");
     }
-    else{
+    else {
         localStorage.setItem("rounds",rounds);
-        window.location.href ="chooser.html";
+        localStorage.setItem("round",1);
+        window.location.href = "chooser.html";
     }
-    
 }
+
+/* Function showRound
+ * Gets rounds and round from local storage and displays in chooser.html.
+ * @param = none
+ * @localStorage => round, rounds
+ * @return = none
+ */
 function showRound(){
-    let statsBox =document.getElementById("statsBox");
-    let message =  
+    let round = localStorage.getItem("round");
+    let rounds = localStorage.getItem("rounds");
+    let statsBox = document.getElementById("statsBox");
+    let message = "Round " + round + " of " + rounds;
     statsBox.innerHTML = message;
 }
 
+/* Function cpuTurn
+ * Called by buttons in chooser with userturn u as argument.
+ * Generates a computer turn c and sends u and c to findWinner
+ * @param = u
+ * @return = u,c
+ */
 function cpuTurn(u){
     let moves = ["r","p","s"];
     let choice = Math.floor(Math.random()*3);
@@ -27,22 +53,30 @@ function cpuTurn(u){
     findWinner(u,c);
 }
 
+/* Function findWinner
+ * Checks if u & c are equal.  If not, finds winner and updates showRound, 
+ * Incrementing round and saving to local storage.
+ * @param = u,c
+ * @localStorage <=> round++
+ * @return = none
+ */
 function findWinner(u,c){
-    if (u==c){
-        alert("we both chose " + u);
+    if (u == c){
+        alert("We both picked " + u);
     }
-    else{
-    let winner = " ";
-    let winArray=[["r","p","I"],["r","s","you"],["p","s","I"],["p","r","you"],["s","r","I"],["s","p","you"]];
+    else {
+        let winner = " ";
+        let winArray=[["r","p","I"],["r","s","you"],["p","s","I"],["p","r","you"],["s","r","I"],["s","p","you"]];
         for (let i = 0; i< winArray.length; i++){
             if (winArray[i][0] == u && winArray[i][1]==c){
                 winner= winArray[i][2];
 
             }
         }
-    alert("You choose " + u + " and I choose " + c + " " + winner + " win!");
+        alert("You choose " + u + " and I choose " + c + " " + winner + " win!");
+        let round = localStorage.getItem("round");
+        round++;
+        localStorage.setItem("round",round);
         showRound();
-    return winner;
-
-}
+    }
 }
